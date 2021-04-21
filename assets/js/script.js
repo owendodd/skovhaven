6a54af5051110a523eff8b3f21f93dc775a56498
Owen Dodd <owen.dodd@gmail.com>
2021-04-21 17:47:13 +0200

    setup landing page

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 50) {
-        $('menu > h1').addClass('collapsed');
-    }
-    else
-    {
-        $('menu > h1').removeClass('collapsed');
-    }
-});
