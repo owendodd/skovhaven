50baa135cb572fb19999b5b2f8c37c619813bafa
Owen Dodd <owen.dodd@gmail.com>
2021-05-03 10:47:31 +0200

    header scripts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,67 @@
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > 50) {
-        $('.menu').fadeOut();
+var menuBox     = $('.menu div'),
+    menuItems   = $('.menu h2').not('.label'),
+    label       = $('.menu h2.label'),
+    headerPoint = $("section.point");
+
+$(window).scroll(function () {
+    if ($(window).scrollTop() > 100) {
+        hideMenu();
     }
-    else
-    {
-        $('.menu').fadeIn();
+    else {
+        showMenu();
+        label.text('menu');
     }
 });
+
+label.click(function () {
+    showMenu();
+});
+
+function hideMenu() {
+    menuItems.addClass('hide');
+    label.removeClass('hide');
+    setTimeout(function () {
+        menuBox.height(44);
+    }, 50);
+};
+
+function showMenu() {
+    menuItems.removeClass('hide');
+    label.addClass('hide');
+    setTimeout(function () {
+        menuBox.height(160);
+    }, 50);
+};
+
+function changeLabel(point) {
+    var     section = $(point),
+            dkTitle = section.find('.dk>h2').text(),
+            enTitle = section.find('.en>h2').text(),
+            dkMenu  = $('.dk>h2.label'),
+            enMenu  = $('.en>h2.label');
+
+            enMenu.text(enTitle);
+            dkMenu.text(dkTitle);
+
+            console.log(section);
+} 
+
+headerPoint.waypoint({
+  handler: function(direction) {
+    if (direction === 'down') {
+        changeLabel(this.element);
+    }
+  },
+  offset: 100
+});
+
+headerPoint.waypoint({
+    handler: function(direction) {
+      if (direction === 'up') {
+          changeLabel(this.element);
+      }
+    },
+    offset: function() {
+        return -this.element.clientHeight
+      }
+  });
